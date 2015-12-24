ff180189b518649e74ee7e528c5b3e04590f576e
gembarrett <gem@gembarrett.com>
2015-12-23 23:56:34 -0500

    added images based on results

--- results.php
+++ results.php
@@ -1,9 +1,7 @@
 <?php
   $long = $_GET['long'];
   $lat = $_GET['lat'];
-  $cEve = 1450915200;
   $cDay = 1451001600;
-  $bDay = 1451088000;
 
   if ($handle = opendir('nope/')) {
 
@@ -18,7 +16,23 @@
   }
 
   $key = file_get_contents($path);
+
+  // test variables with alaska
+  // $lat = "61.2180556";
+  // $long = "-149.9002778";
+
   $url = "https://api.forecast.io/forecast/".$key."/".$lat.",".$long.",".$cDay;
   $contents = file_get_contents($url);
-  echo $contents;
+  $json = json_decode($contents, true);
+  // var_dump($json);
+  function in_array_r($needle, $haystack, $strict = true) {
+      foreach ($haystack as $item) {
+          if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+              return true;
+          }
+      }
+
+      return false;
+  }
+  echo in_array_r("snow", $json) ? '<img src="GC-yes.jpg">' : '<img src="GC-no.jpg">';
 ?>
